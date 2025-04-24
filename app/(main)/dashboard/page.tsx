61d7c576157b875a009cb7c3a3b5031cd97193f4
Melvunx <augustine.melv@gmmail.com>
2025-04-24 15:59:58 +0200

    setup Offer form

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,12 +1,13 @@
-import { authSession } from "@/src/lib/auth-session";
+import { authSession, getRequiredUser } from "@/src/lib/auth-session";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
   const session = await authSession();
+  const user = await getRequiredUser();
 
-  if (!session.authorization) {
-    redirect(session.redirect);
-  }
+  if (!session.authorization) redirect(session.redirect);
+
+  if ("redirect" in user) redirect(user.redirect);
 
   return <div>Dashboard</div>;
 }
